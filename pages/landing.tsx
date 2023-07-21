9d3a6bd7639a8f90ec00d782fdd156159b9ad4ac
xentoshi <xentoshi@protonmail.com>
2023-07-21 17:53:52 +0900

    Update landing.tx

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -97,18 +97,7 @@ import {
               boxShadow={'2xl'}
               width={'full'}
               overflow={'hidden'}>
-              <IconButton
-                aria-label={'Play Button'}
-                variant={'ghost'}
-                _hover={{ bg: 'transparent' }}
-                icon={<PlayIcon w={12} h={12} />}
-                size={'lg'}
-                color={'white'}
-                position={'absolute'}
-                left={'50%'}
-                top={'50%'}
-                transform={'translateX(-50%) translateY(-50%)'}
-              />
+    
               <Image
                 alt={'Hero Image'}
                 fit={'cover'}
@@ -116,7 +105,7 @@ import {
                 w={'100%'}
                 h={'100%'}
                 src={
-                  'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+                  'https://images.unsplash.com/photo-1611262588019-db6cc2032da3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
                 }
               />
             </Box>
