5161697f0e23ceea5fd1bee5dbba44c24e84af8a
xentoshi <xentoshi@protonmail.com>
2023-07-22 01:21:26 +0900

    Added powered by container

--- pages/features.tsx
+++ pages/features.tsx
@@ -1,37 +1,51 @@
+import { Box, Grid, chakra, Text, Flex } from '@chakra-ui/react';
 
-import { ReactElement } from 'react';
-import { Box, SimpleGrid, Avatar } from '@chakra-ui/react';
+const ResponsiveLogo = chakra('img', {
+  baseStyle: {
+    width: '100px',
+    height: '100px',
+    rounded: 'full', // Make the logos rounded
+  },
+  sizes: {
+    md: {
+      width: '150px',
+      height: '150px',
+    },
+  },
+});
 
-interface FeatureProps {
-  title: string;
-  text: string;
-  icon: ReactElement;
-}
-
-
-export default function SimpleThreeColumns() {
+export default function ThreeLogosGrid() {
   return (
+
     <Box p={4}>
-      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
-      <Avatar
-          src="https://assets-global.website-files.com/6364e65656ab107e465325d2/63f6948a05c1410a41c86cf6_0Sqbm5PxjFpSYWRPsnbdsO4zHmLnlVUcu218_rUy100.png"
-          size="150px"
-          position="relative"
-          top={0}
-        />
-            <Avatar
-          src="https://storage.googleapis.com/ethglobal-api-production/organizations%2Fvxwti%2Flogo%2F1678645512720_Screen%20Shot%202023-03-12%20at%2011.25.01%20AM.png"
-          size="150px"
-          position="relative"
-          top={0}
-        />
-            <Avatar
-          src="https://101-user-uploads.s3.us-east-1.amazonaws.com/a9dfa84d-b9ed-4ae3-802f-5c4dc66c8bd5.png"
-          size="150px"
-          position="relative"
-          top={0}
-        />
-      </SimpleGrid>
+     <Flex direction="column" alignItems="center" mb={4}> {/* Add margin-bottom to create distance */}
+        <Text fontSize="sm" mt={5}>Powered by</Text>
+      </Flex>
+      <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }} gap={8}>
+        <Flex direction="column" alignItems="center">
+          <ResponsiveLogo
+            src="https://assets-global.website-files.com/6364e65656ab107e465325d2/63f6948a05c1410a41c86cf6_0Sqbm5PxjFpSYWRPsnbdsO4zHmLnlVUcu218_rUy100.png"
+            alt="WeaveDB"
+          />
+          <Text mt={5}>WeaveDB takes your messages cross-chain and stores them in an unhackable decentralized database 💎</Text>
+        </Flex>
+
+        <Flex direction="column" alignItems="center">
+          <ResponsiveLogo
+            src="https://storage.googleapis.com/ethglobal-api-production/organizations%2Fvxwti%2Flogo%2F1678645512720_Screen%20Shot%202023-03-12%20at%2011.25.01%20AM.png"
+            alt="Lens protocol"
+          />
+          <Text mt={5}>Lens Protocol's social graph is giving you a bird's-eye view of your community 🌏 </Text>
+        </Flex>
+
+        <Flex direction="column" alignItems="center">
+          <ResponsiveLogo
+            src="https://101-user-uploads.s3.us-east-1.amazonaws.com/a9dfa84d-b9ed-4ae3-802f-5c4dc66c8bd5.png"
+            alt="Lit protocol"
+          />
+          <Text mt={5}>Lit protocol makes sure that only invited guests enter your chat ✅</Text>
+        </Flex>
+      </Grid>
     </Box>
   );
 }
--- pages/landing.tsx
+++ pages/landing.tsx
@@ -20,181 +20,183 @@ import Features from './features'
   export default function CallToActionWithVideo() {
     return (
       <Container maxW={'container.xl'}>
-      <Stack
-        spacing={{ base: 5, md: 5 }}
-        align={{ base: 'center', md: 'flex-start' }}
-        direction={{ base: 'column', md: 'row' }}
-        py={{ base: 1, md: 10 }}>
-          {/* Input for "Wallet Address" */}
-          <InputGroup size="lg" w={{ base: '100%', md: '70%' }} alignSelf="center">
-            <InputLeftAddon
-              pointerEvents="none"
-              fontSize={{ base: 'sm', md: 'md' }}
-              color="gray.600"
-              bg="white"
-              border="1px solid"
-              borderColor={useColorModeValue('gray.200', 'gray.700')}
-              rounded="xl"
-              px={4}
-              py={3}
-              fontWeight="semibold"
-              whiteSpace="nowrap"
-              shadow="md">
-              ETH wallet address
-            </InputLeftAddon>
-            <Input
-              type="wallet"
-              placeholder="0xEc86f5..."
-              rounded="xl"
-              py={4}
-              px={6}
-              bg={useColorModeValue('white', 'gray.800')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}
-              borderWidth={1}
-              _focus={{
-                borderColor: useColorModeValue('purple.500', 'purple.300'),
-                boxShadow: 'outline',
-              }}
-            />
-          </InputGroup>
-          {/* Input for "Choose topic" */}
-          <InputGroup size="lg" w={{ base: '100%', md: '70%' }} alignSelf="center">
-        <InputLeftAddon
-          pointerEvents="none"
-          fontSize={{ base: 'sm', md: 'md' }}
-          color="gray.600"
-          bg="white"
-          border="1px solid"
-          borderColor={useColorModeValue('gray.200', 'gray.700')}
-          rounded="xl"
-          px={4}
-          py={3}
-          fontWeight="semibold"
-          whiteSpace="nowrap"
-          shadow="md">
-          Choose topic
-        </InputLeftAddon>
-        <Input
-          type="text"
-          placeholder="Enter your topic..."
-          rounded="xl"
-          py={4}
-          px={6}
-          bg={useColorModeValue('white', 'gray.800')}
-          borderColor={useColorModeValue('gray.200', 'gray.700')}
-          borderWidth={1}
-          _focus={{
-            borderColor: useColorModeValue('purple.500', 'purple.300'),
-            boxShadow: 'outline',
-          }}
-        />
-          </InputGroup>
-        <Button
-                rounded={'full'}
-                size={'lg'}
-                fontWeight={'semibold'}
-                px={8}
-                colorScheme={'pink'}
-                bg={'rgb(249, 92, 255)'}
-                _hover={{ bg: '#b900c9' }}>
-                Join chat
-              </Button>
-        </Stack>
-        <Stack
-          align={'center'}
-          spacing={{ base: 8, md: 10 }}
-          py={{ base: 1, md: 1 }}
-          direction={{ base: 'column', md: 'row' }}>
-          <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-            <Heading
-              lineHeight={1.1}
-              fontWeight={600}
-              fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
-              <Text
-                as={'span'}
-                position={'relative'}
-                _after={{
-                  content: "''",
-                  width: 'full',
-                  height: '30%',
-                  position: 'absolute',
-                  bottom: 1,
-                  left: 0,
-                  bg: 'rgb(249, 92, 255)',
-                  zIndex: -1,
-                }}>
-                Connect,
-              </Text>
-              <br />
-              <Text as={'span'} color={'rgb(249, 92, 255)'}>
-                with people that inspire you! 💬
-              </Text>
-            </Heading>
-            <Text color={'gray.500'}>
-              Scroll through their Lens feed and send them direct, private message. 
-            </Text>
-            <Stack
-              spacing={{ base: 4, sm: 6 }}
-              direction={{ base: 'column', sm: 'row' }}>
-              <Button
-                rounded={'full'}
-                size={'lg'}
-                fontWeight={'normal'}
+          <Container maxW={'container.xl'}>
+          <Stack
+            spacing={{ base: 5, md: 5 }}
+            align={{ base: 'center', md: 'flex-start' }}
+            direction={{ base: 'column', md: 'row' }}
+            py={{ base: 1, md: 10 }}>
+              {/* Input for "Wallet Address" */}
+            <InputGroup size="lg" w={{ base: '100%', md: '70%' }} alignSelf="center">
+              <InputLeftAddon
+                pointerEvents="none"
+                fontSize={{ base: 'sm', md: 'md' }}
+                color="gray.600"
+                bg="white"
+                border="1px solid"
+                borderColor={useColorModeValue('gray.200', 'gray.700')}
+                rounded="xl"
+                px={4}
+                py={3}
+                fontWeight="semibold"
+                whiteSpace="nowrap"
+                shadow="md">
+                ETH wallet address
+              </InputLeftAddon>
+              <Input
+                type="wallet"
+                placeholder="0xEc86f5..."
+                rounded="xl"
+                py={4}
                 px={6}
-                colorScheme={'red'}
-                bg={'rgb(249, 92, 255)'}
-                _hover={{ bg: '#b900c9' }}>
-                Get started
-              </Button>
-              <Button
-                rounded={'full'}
-                size={'lg'}
-                fontWeight={'normal'}
-                px={6}
-                leftIcon={<PlayIcon h={4} w={4} color={'gray.300'} />}>
-                How It Works
-              </Button>
-            </Stack>
-            <Features />
-          </Stack>
-          <Flex
-            flex={1}
-            justify={'center'}
-            align={'center'}
-            position={'relative'}
-            w={'full'}>
-            <Blob
-              w={'150%'}
-              h={'150%'}
-              position={'absolute'}
-              top={'-30%'}
-              left={0}
-              zIndex={-1}
-              color={useColorModeValue('rgba(185, 0, 201, 0.5)', '#b900c9')}
-
-              
-            />
-            <Box
-              position={'relative'}
-              height={'300px'}
-              rounded={'2xl'}
-              boxShadow={'2xl'}
-              width={'full'}
-              overflow={'hidden'}>
-    
-              <Image
-                alt={'Hero Image'}
-                fit={'cover'}
-                align={'center'}
-                w={'100%'}
-                h={'100%'}
-                src={
-                  'https://images.unsplash.com/photo-1611262588019-db6cc2032da3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
-                }
+                bg={useColorModeValue('white', 'gray.800')}
+                borderColor={useColorModeValue('gray.200', 'gray.700')}
+                borderWidth={1}
+                _focus={{
+                  borderColor: useColorModeValue('purple.500', 'purple.300'),
+                  boxShadow: 'outline',
+                }}
               />
-            </Box>
-          </Flex>
-        </Stack>
+            </InputGroup>
+            {/* Input for "Choose topic" */}
+          <InputGroup size="lg" w={{ base: '100%', md: '65%' }} alignSelf="center">
+          <InputLeftAddon
+            pointerEvents="none"
+            fontSize={{ base: 'sm', md: 'md' }}
+            color="gray.600"
+            bg="white"
+            border="1px solid"
+            borderColor={useColorModeValue('gray.200', 'gray.700')}
+            rounded="xl"
+            px={4}
+            py={3}
+            fontWeight="semibold"
+            whiteSpace="nowrap"
+            shadow="md">
+            Choose topic
+          </InputLeftAddon>
+          <Input
+            type="text"
+            placeholder="Enter your topic..."
+            rounded="xl"
+            py={4}
+            px={6}
+            bg={useColorModeValue('white', 'gray.800')}
+            borderColor={useColorModeValue('gray.200', 'gray.700')}
+            borderWidth={1}
+            _focus={{
+              borderColor: useColorModeValue('purple.500', 'purple.300'),
+              boxShadow: 'outline',
+            }}
+          />
+            </InputGroup>
+            <Button
+                    rounded={'full'}
+                    size={'lg'}
+                    fontWeight={'semibold'}
+                    px={10}
+                    colorScheme={'pink'}
+                    bg={'rgb(249, 92, 255)'}
+                    _hover={{ bg: '#b900c9' }}>
+                    Join chat
+                  </Button>
+            </Stack>
+            <Stack
+              align={'center'}
+              spacing={{ base: 8, md: 10 }}
+              py={{ base: 1, md: 1 }}
+              direction={{ base: 'column', md: 'row' }}>
+              <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+                <Heading
+                  lineHeight={1.1}
+                  fontWeight={600}
+                  fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
+                  <Text
+                    as={'span'}
+                    position={'relative'}
+                    _after={{
+                      content: "''",
+                      width: 'full',
+                      height: '30%',
+                      position: 'absolute',
+                      bottom: 1,
+                      left: 0,
+                      bg: 'rgb(249, 92, 255)',
+                      zIndex: -1,
+                    }}>
+                    Connect,
+                  </Text>
+                  <br />
+                  <Text as={'span'} color={'rgb(249, 92, 255)'}>
+                    with people that inspire you! 💬
+                  </Text>
+                </Heading>
+                <Text color={'gray.500'}>
+                  Scroll through their Lens feed and send them direct, private message. 
+                </Text>
+                <Stack
+                  spacing={{ base: 4, sm: 6 }}
+                  direction={{ base: 'column', sm: 'row' }}>
+                  <Button
+                    rounded={'full'}
+                    size={'lg'}
+                    fontWeight={'normal'}
+                    px={6}
+                    colorScheme={'red'}
+                    bg={'rgb(249, 92, 255)'}
+                    _hover={{ bg: '#b900c9' }}>
+                    Get started
+                  </Button>
+                  <Button
+                    rounded={'full'}
+                    size={'lg'}
+                    fontWeight={'normal'}
+                    px={6}
+                    leftIcon={<PlayIcon h={4} w={4} color={'gray.300'} />}>
+                    How It Works
+                  </Button>
+                </Stack>
+              </Stack>
+              <Flex
+                flex={1}
+                justify={'center'}
+                align={'center'}
+                position={'relative'}
+                w={'full'}>
+                <Blob
+                  w={'150%'}
+                  h={'150%'}
+                  position={'absolute'}
+                  top={'-30%'}
+                  left={0}
+                  zIndex={-1}
+                  color={useColorModeValue('rgba(185, 0, 201, 0.5)', '#b900c9')}
+                />
+                <Box
+                  position={'relative'}
+                  height={'300px'}
+                  rounded={'2xl'}
+                  boxShadow={'2xl'}
+                  width={'full'}
+                  overflow={'hidden'}>
+        
+                  <Image
+                    alt={'Hero Image'}
+                    fit={'cover'}
+                    align={'center'}
+                    w={'100%'}
+                    h={'100%'}
+                    src={
+                      'https://images.unsplash.com/photo-1611262588019-db6cc2032da3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
+                    }
+                  />
+                </Box>
+              </Flex>
+            </Stack>
+          </Container>
+          <Container maxW={'container.xl'} py={100} textAlign="center">
+          <Features />
+          </Container>
       </Container>
     );
   }
